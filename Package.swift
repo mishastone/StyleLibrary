ea9f509467b348d60fff1859922cb54dcacefb72
Misha Stone <misha@paypiip.com>
2021-01-16 15:07:00 -0500

    Added Neumorphic Style resource

--- Package.swift
+++ Package.swift
@@ -24,7 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "StyleLibrary",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .copy("Neumorphic")
+            ]
+            ),
         .testTarget(
             name: "StyleLibraryTests",
             dependencies: ["StyleLibrary"]),
