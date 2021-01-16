b0aeb18fd2f727450a8e765ad0cab8cb722a6b80
Misha Stone <misha@paypiip.com>
2021-01-16 14:40:44 -0500

    Neumorphic Style added

---
+++ Sources/StyleLibrary/Neumorphic/Neumorphic-Styles-SwiftUI.swift
@@ -0,0 +1,56 @@
+//
+//  File.swift
+//  
+//
+//  Created by Misha L. G. Stone on 2021/01/16.
+//
+
+import Foundation
+import SwiftUI
+
+//MARK: Buttons
+struct LightNeumorphicStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .contentShape(Circle())
+            .background(
+                Group {
+                    if configuration.isPressed {
+                        Circle()
+                            .fill(Color.offWhite)
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.gray, lineWidth: 4)
+                                    .blur(radius: 4)
+                                    .offset(x: 2, y: 2)
+                                    .mask(Circle().fill(LinearGradient(Color.black, Color.clear)))
+                            )
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 8)
+                                    .blur(radius: 4)
+                                    .offset(x: -2, y: -2)
+                                    .mask(Circle().fill(LinearGradient(Color.clear, Color.black)))
+                            )
+                    } else {
+                        Circle()
+                            .fill(Color.offWhite)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                            .shadow(color: Color.white.opacity(0.7), radius: 10, x: 05, y: -5)
+                    }
+                }
+            )
+    }
+}
+
+struct DarkNeumorphicStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .contentShape(Circle())
+            .background(
+                DarkBackgroundNeumorphic(isHighlighted: configuration.isPressed, shape: Circle())
+            )
+    }
+}
---
+++ Sources/StyleLibrary/Neumorphic/Neumorphic-Views-SwiftUI.swift
@@ -0,0 +1,129 @@
+//
+//  File.swift
+//  
+//
+//  Created by Misha L. G. Stone on 2021/01/16.
+//
+
+import Foundation
+import SwiftUI
+
+
+//MARK: Backgrounds
+
+struct DarkBackgroundNeumorphic<S: Shape>: View {
+    var isHighlighted: Bool
+    var shape: S
+    
+    var body: some View {
+        ZStack {
+            if isHighlighted {
+                shape
+                    .fill(LinearGradient(Color.lightEnd, Color.lightStart))
+                    .overlay(shape.stroke(LinearGradient(Color.lightStart, Color.lightEnd), lineWidth: 4))
+                    .shadow(color: Color.darkStart, radius: 10, x: 5, y: 5)
+                    .shadow(color: Color.darkEnd, radius: 10, x: -5, y: -5)
+            } else {
+                shape
+                    .fill(LinearGradient(Color.darkStart, Color.darkEnd))
+                    .overlay(shape.stroke(Color.lightStart, lineWidth: 4))
+                    .shadow(color: Color.darkStart, radius: 10, x: -10, y: -10)
+                    .shadow(color: Color.darkEnd, radius: 10, x: 10, y: 10)
+            }
+        }
+    }
+}
+
+//MARK: Buttons
+
+struct IconNeumorphicButton: View {
+    //Toggle between the color scheme of the device
+    @Environment(\.colorScheme) var colorScheme
+    var image: String
+    var foregroundColor: Color?
+    var action: (() -> Void)?
+    
+    private var schemedButton: some View {
+        if colorScheme == .light {
+            return AnyView(LightIconButton(image, foregroundColor, action: action))
+        } else {
+            return AnyView(DarkIconButton(image, foregroundColor, action: action))
+        }
+    }
+    
+    init(_ image: String, foregroundColor: Color? = Color.gray, action: (() -> Void)? = nil) {
+        self.image = image
+        self.foregroundColor = foregroundColor ?? Color.gray
+        self.action = action
+    }
+    
+    var body: some View {
+        schemedButton
+    }
+}
+
+// Icon Button Dependencies
+struct LightIconButton: View {
+    var image: String
+    var foregroundColor: Color
+    var action: (() -> Void)?
+    
+    init(_ image: String, _ foregroundColor: Color? = nil, action: (() -> Void)? = nil) {
+        self.image = image
+        self.foregroundColor = foregroundColor ?? Color.gray
+        self.action = action
+    }
+    
+    var body: some View {
+        Button (action: {
+            guard let getAction = action else {
+                return
+            }
+            
+            getAction()
+        }) {
+            
+            if #available(OSX 11.0, *) {
+                Image(systemName: image)
+                    .foregroundColor(foregroundColor)
+            } else {
+                // Fallback on earlier versions
+                Image(image)
+                    .foregroundColor(foregroundColor)
+            }
+        }
+        .buttonStyle(LightNeumorphicStyle())
+    }
+}
+
+struct DarkIconButton: View {
+    var image: String
+    var foregroundColor: Color
+    var action: (() -> Void)?
+    
+    init(_ image: String, _ foregroundColor: Color? = nil, action: (() -> Void)? = nil) {
+        self.image = image
+        self.foregroundColor = foregroundColor ?? Color.gray
+        self.action = action
+    }
+    
+    var body: some View {
+        Button(action: {
+            guard let getAction = action else {
+                return
+            }
+            
+            getAction()
+        }) {
+            if #available(OSX 11.0, *) {
+                Image(systemName: image)
+                    .foregroundColor(foregroundColor)
+            } else {
+                // Fallback on earlier versions
+                Image(image)
+                    .foregroundColor(foregroundColor)
+            }
+        }
+        .buttonStyle(DarkNeumorphicStyle())
+    }
+}
