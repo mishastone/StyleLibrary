f315529d69cb1004744bdf6a174dd4d59090fdb8
Misha Stone <misha@paypiip.com>
2021-01-16 21:17:07 -0500

    Neumorphic

--- Package.swift
+++ Package.swift
@@ -24,11 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "StyleLibrary",
-            dependencies: [],
-            resources: [
-                .copy("StyleLibrary/Neumorphic")
-            ]
-            ),
+            dependencies: []),
         .testTarget(
             name: "StyleLibraryTests",
             dependencies: ["StyleLibrary"]),
